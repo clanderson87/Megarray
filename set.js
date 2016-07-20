d7c5a40e1f5ba038de77f1994c410399a4b1826d
Chris Anderson <cl.anderson8@gmail.com>
2016-07-20 10:21:59 -0500

    Working on resultType. Maybe too tired to code.

--- set.js
+++ set.js
@@ -90,13 +90,19 @@ window.Set = function() {
     returnTypes: function(){
       var result = [];
 
+
       for (var index = 0; index < this.length; index++) {
         var element = this[index];
         if(!result.includes(typeof(element))){
           result.push(typeof(element));
+          console.log("result is: ", result);
+          result.push(1);
+        } else {
+          var indexToChange = (result.indexOf(element) + 1);
+          var toChangeTo = (result[indexToChange] + 1);
+          result[indexToChange] = toChangeTo;
         }
       }
-
       return result;
     },
 
